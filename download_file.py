e664dac0f005233ffae4f867d6c6f74437c08af7
Cuong Ngo <bavuonglong93@gmail.com>
2020-02-09 22:33:59 +0100

    download file and save it locally

---
+++ download_file.py
@@ -0,0 +1,16 @@
+#! python3
+
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+
+print(res.status_code)
+
+print(res.raise_for_status)
+
+playFile = open('RomeoAndJuliet.txt', 'wb')
+
+for chunk in res.iter_content(100000):
+	playFile.write(chunk)
+
+playFile.close()
